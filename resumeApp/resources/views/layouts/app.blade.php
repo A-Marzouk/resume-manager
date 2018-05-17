dd52a3092cbbe9c35c083c60769c4065b34cc51f
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-05-17 13:43:05 +0300

    add favicon

--- resumeApp/resources/views/layouts/app.blade.php
+++ resumeApp/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">

dd25349b4060ba34cd332d37d58b7b00554ad9d3
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-05-18 11:09:42 +0300

    add route to clear-cache

--- resumeApp/routes/web.php
+++ resumeApp/routes/web.php
@@ -35,7 +35,10 @@ Route::prefix('freelancer')->group(function (){
     Route::post('/register/submit','Auth\RegisterController@register')->name('freelancer.register.submit');
 });
 
-
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return redirect()->back();
+});
 
 // admin area :
 Route::get('/admin','AdminsController@welcomePage')->name('admin.dashboard');

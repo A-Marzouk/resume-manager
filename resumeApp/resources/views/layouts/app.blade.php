cdabe1a75789b381fdd984a8d8ae398cee2f95c2
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-05-18 12:01:17 +0300

    edit some paths

--- resumeApp/resources/views/layouts/app.blade.php
+++ resumeApp/resources/views/layouts/app.blade.php
@@ -13,20 +13,20 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="favicon.png"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="/resumeApp/resources/views/customTheme/css/freelancerForm.css" rel="stylesheet">
+    <link href="resumeApp/resources/views/customTheme/css/freelancerForm.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="/resumeApp/resources/views/customTheme/images/logo123.png" width="5%"> Resume Editor
+                    <img src="resumeApp/resources/views/customTheme/images/logo123.png" width="5%"> Resume Editor
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -74,6 +74,6 @@
             @yield('content')
         </main>
     </div>
-    <script src="/resumeApp/resources/views/customTheme/js/main.js"></script>
+    <script src="resumeApp/resources/views/customTheme/js/main.js"></script>
 </body>
 </html>

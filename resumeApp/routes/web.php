91d3406ccc480805dcf159f66e184caeb3ec664c
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-05-18 11:26:12 +0300

    add route to clear-cache 2

--- resumeApp/routes/web.php
+++ resumeApp/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
@@ -36,7 +37,7 @@ Route::prefix('freelancer')->group(function (){
 });
 
 Route::get('/clear-cache', function() {
-    $exitCode = Artisan::call('cache:clear');
+    Artisan::call('cache:clear');
     return redirect()->back();
 });
 

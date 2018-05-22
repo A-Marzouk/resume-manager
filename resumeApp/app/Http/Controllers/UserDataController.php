7f9c1f5a59736b366c544a9b78cec88b247662ce
Ahmed <perfect_ahmed@yahoo.com>
2018-05-22 13:03:18 +0300

    enable telegram notifications

--- resumeApp/app/Http/Controllers/UserDataController.php
+++ resumeApp/app/Http/Controllers/UserDataController.php
@@ -87,7 +87,7 @@ class UserDataController extends Controller
             }
             $userData->save();
 
-//                $this->sendTelegram();
+                $this->sendTelegram();
 //                $this->sendNotification();
             return redirect('/freelancer')->with('successMessage', 'Your changes have been successfully saved.');
         }else{

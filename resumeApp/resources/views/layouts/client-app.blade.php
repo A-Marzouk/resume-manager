573e57ccc6dd6c653a6a9af6c17b1fe9e4dea608
Ahmed <perfect_ahmed@yahoo.com>
2018-05-29 16:14:02 +0300

    Client register page 2

--- resumeApp/resources/views/layouts/client-app.blade.php
+++ resumeApp/resources/views/layouts/client-app.blade.php
@@ -25,7 +25,7 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand col-md-3 col-9" href="{{ url('/') }}">
                 <img src="/resumeApp/resources/views/customTheme/images/newResume/123wf_logo.png" alt="logo">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

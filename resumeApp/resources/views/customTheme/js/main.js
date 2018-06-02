b32ba4b5089a18c54f3e69644673d87c0c6f9acb
Ahmed <perfect_ahmed@yahoo.com>
2018-06-02 14:11:17 +0300

    freelancer form 2

--- resumeApp/resources/views/customTheme/js/main.js
+++ resumeApp/resources/views/customTheme/js/main.js
@@ -215,4 +215,17 @@ $(document).ready(function () {
         });
     });
 
+
+    // when item is active :
+    $(function(){
+        var hash = window.location.hash;
+        hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+        $('.nav-tabs a').click(function (e) {
+            $(this).tab('show');
+            var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+            window.location.hash = this.hash;
+            $('html,body').scrollTop(scrollmem);
+        });
+    });
 });

be179a3c1d784f386c3ecaa65b0c509d2b3d0a98
Ahmed <perfect_ahmed@yahoo.com>
2018-06-06 17:17:08 +0300

    e-mail pages title

--- resumeApp/config/app.php
+++ resumeApp/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Resume Manager'),
+    'name' => env('APP_NAME', '123 Workforce'),
 
     /*
     |--------------------------------------------------------------------------
--- resumeApp/resources/views/layouts/app.blade.php
+++ resumeApp/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Resume Manager</title>
+    <title>123 Workforce</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>

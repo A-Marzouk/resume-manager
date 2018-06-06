1cd8933f13cd3bd26e961dde6954303e52fe2ad8
Ahmed <perfect_ahmed@yahoo.com>
2018-06-06 17:13:17 +0300

    e-mail edit 3

--- resumeApp/resources/views/emails/freelancer_welcome.blade.php
+++ resumeApp/resources/views/emails/freelancer_welcome.blade.php
@@ -14,11 +14,11 @@
 
                         We are now actively promoting your cv to our list of clients currently hiring. <br/><br/>We are currently seeking a suitable company for you to work at for your preferred hourly rate and availability, will be in touch with you as soon as we have a good job offer for you.
 
-                        <br/><br/>Please feel free to contact us at anytime with questions that you could have on riz@123workforce.com.  Could we also ask you to please edit your availability of hours should it change over time by signing in and edit hours. With this, we can accurately place and match  you with the most suitable clients.
+                        <br/><br/>Please feel free to contact us at anytime with questions that you could have on <b>riz@123workforce.com</b>.  Could we also ask you to please edit your availability of hours should it change over time by signing in and edit hours. With this, we can accurately place and match  you with the most suitable clients.
 
                     </p>
                     <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                        Wishing you ongoing success with your current projects !<br/>
+                        Wishing you onging success with your current projects !<br/>
                         Kindest Regards,<b>123 Workforce</b></p>
                 </td>
             </tr>

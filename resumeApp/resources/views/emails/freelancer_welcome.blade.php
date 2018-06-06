34933f9a89b11c2dc94157b29c5d164a1d25b61d
Ahmed <perfect_ahmed@yahoo.com>
2018-06-06 17:08:33 +0300

    e-mail edit

--- resumeApp/resources/views/emails/freelancer_welcome.blade.php
+++ resumeApp/resources/views/emails/freelancer_welcome.blade.php
@@ -12,30 +12,11 @@
 
                         We don't require any further interviews or documents from you at this time.
 
-                        We are now actively promoting your cv to our list of clients currently hiring. <br/>We are currently seeking a suitable company for you to work at for your preferred hourly rate and availability, will be in touch with you as soon as we have a good job offer for you.
+                        We are now actively promoting your cv to our list of clients currently hiring. <br/><br/>We are currently seeking a suitable company for you to work at for your preferred hourly rate and availability, will be in touch with you as soon as we have a good job offer for you.
 
                         <br/><br/>Please feel free to contact us at anytime with questions that you could have on riz@123workforce.com.  Could we also ask you to please edit your availability of hours should it change over time by signing in and edit hours. With this, we can accurately place and match  you with the most suitable clients.
 
                     </p>
-                    <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; padding: 0; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-                        <tr>
-                            <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                <table width="100%" border="0" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                    <tr>
-                                        <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                            <table border="0" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                                <tr>
-                                                    <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                                        <a href="https://123workforce.com/{{$username}}"; class="button button-blue" target="_blank" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3097D1; border-top: 10px solid #3097D1; border-right: 18px solid #3097D1; border-bottom: 10px solid #3097D1; border-left: 18px solid #3097D1;">View Resume</a>
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
                     <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
                         Wishing you ongoing success with your current projects !<br/>
                         Kindest Regards, 123 Workforce</p>

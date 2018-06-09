ad07cfb0c005aeb719151d3d84d0b2d252e8b61f
Ahmed <perfect_ahmed@yahoo.com>
2018-06-09 15:54:29 +0300

    forgot password system 3

--- resumeApp/app/Http/Controllers/Auth/ClientForgotPasswordController.php
+++ resumeApp/app/Http/Controllers/Auth/ClientForgotPasswordController.php
@@ -8,7 +8,7 @@
 
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Support\Facades\Password;
 

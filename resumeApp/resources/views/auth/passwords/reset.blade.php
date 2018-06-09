63aa362c037d9a94c6e138e8039dde911dd0e538
A-Marzouk <perfect_ahmed@yahoo.com>
2018-06-09 15:22:57 +0300

    reset form styles

--- resumeApp/resources/views/auth/passwords/reset.blade.php
+++ resumeApp/resources/views/auth/passwords/reset.blade.php
@@ -15,7 +15,7 @@
                         <div class="form-group">
                             <label for="email" class="formLabel">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="email" type="email" class="panelFormInput form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -26,10 +26,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group">
                             <label for="password" class="formLabel">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="password" type="password" class="panelFormInput form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -40,11 +40,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="form-group">
+                            <label for="password-confirm" class="formLabel">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <div class="">
+                                <input id="password-confirm" type="password" class="form-control panelFormInput" name="password_confirmation" required>
                             </div>
                         </div>
 

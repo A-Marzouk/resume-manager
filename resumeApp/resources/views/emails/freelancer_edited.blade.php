2e8e47181b763aa7c9511d38cb9f998adfefd3c0
Ahmed <perfect_ahmed@yahoo.com>
2018-06-14 13:03:44 +0300

    email designs 3

--- resumeApp/resources/views/emails/freelancer_edited.blade.php
+++ resumeApp/resources/views/emails/freelancer_edited.blade.php
@@ -7,7 +7,7 @@
         Please note that, <br>
         User <b>{{auth()->user()->firstName}} {{auth()->user()->lastName}}</b> has updated his profile !
     </div>
-    <div class="buttonMain col-md-4 offset-4">
+    <div class="buttonMain col-md-4 offset-md-4">
         <a href="123workforce.magictimeapps.com/{{auth()->user()->username}}"; class="hireBtn btn-block" target="_blank">View Resume</a>
     </div>
 @endsection

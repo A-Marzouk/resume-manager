f6202edea4d51e479f1318448bd39a070c17a80a
Ahmed <perfect_ahmed@yahoo.com>
2018-06-19 11:12:27 +0300

    chat with visitors ( little bit organized code )

--- resumeApp/app/Http/Controllers/ChatController.php
+++ resumeApp/app/Http/Controllers/ChatController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 class ChatController extends Controller
 {
     protected $user ;
+    protected $var_id;
 
     public function getMessages(){
         return Message::with('user','visitor')->get();
@@ -22,44 +23,53 @@ class ChatController extends Controller
     }
 
     public function storeMessages(Request $request){
-        $user = auth()->user();
-        $message = new Message;
-        $message->message = $request->message;
-        if($user){
-            if($user->admin == 1){
-                $user = User::where('username','admin_workforce')->first();
-            }
-            $message->user_id = $user->id;
-        }else{
-            // if I have the session set so no need to new user, else make a new one !
-            $token = session()->get('visitToken');
-            if(!empty($token)){
-                $visitor = Visitor::where('token',$token)->first() ;
-                $message->visitor_id = $visitor->id;
-                $user = $visitor;
-            }else{
-                $visitor = new Visitor;
-                $visitor->firstName = 'Visitor';
-                $visitor->token = str_random(16);
-                $visitor->save();
-                $visitor->firstName = 'Visitor.'.$visitor->id;
-                $visitor->save();
-                // save token in session
-                session()->put('visitToken',$visitor->token);
-                $message->visitor_id = $visitor->id;
-                $user = $visitor;
-            }
+        $this->setCurrentUser();
 
-        }
+        $message                  = new Message;
+        $message->message         = $request->message;
+        $message->{$this->var_id} = $this->user->id;
 
         $message->save();
 
         // trigger an event to announce that a message has been posted !
 
-        event(new MessagePosted($message,$user));
+        event(new MessagePosted($message,$this->user));
 
         return [
             'status' => 'ok'
         ];
+    }
+
+    public function setCurrentUser(){
+        $user = auth()->user();
+        if($user){
+            if($user->admin == 1){
+                $user = User::where('username','admin_workforce')->first();
+            }
+            $this->user   = $user;
+            $this->var_id = 'user_id';
+        }else{ // a visitor
+            $token = session()->get('visitToken2');
+            if(!empty($token)){
+                $visitor = Visitor::where('token',$token)->first() ;
+            }else{
+               $visitor = $this->createNewVisitor();
+                // save token in session
+                session()->put('visitToken2',$visitor->token);
+            }
+            $this->user   = $visitor;
+            $this->var_id = 'visitor_id';
+        }
+    }
+
+    public function createNewVisitor(){
+        $visitor = new Visitor;
+        $visitor->firstName = 'Visitor';
+        $visitor->token = str_random(16);
+        $visitor->save();
+        $visitor->firstName = 'Visitor.'.$visitor->id;
+        $visitor->save();
+
+        return $visitor;
     }
 }

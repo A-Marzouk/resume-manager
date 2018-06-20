3b9f392fb8b8835ff9ff1f7930e33616d91c9c72
Ahmed <perfect_ahmed@yahoo.com>
2018-06-20 11:30:26 +0300

    okay with user, some crushes in admin area will be fixed when adding clients.

--- resumeApp/app/Http/Controllers/ChatController.php
+++ resumeApp/app/Http/Controllers/ChatController.php
@@ -65,18 +65,31 @@ class ChatController extends Controller
         if($user){
             if($user->admin == 1){
                 $user = User::where('username','admin_workforce')->first();
+            }else{
+                // our user !
+                if(!empty(session()->get('userConversationId'))){
+                    $this->conversation = Conversation::where('id',session()->get('userConversationId'))->first();
+                }else{
+                    $userConversation = new Conversation;
+                    $userConversation->user_id = $user->id;
+                    $userConversation->save();
+                    session()->put('userConversationId',$userConversation->id);
+                    $this->conversation = $userConversation;
+                }
             }
+
             $this->user   = $user;
             $this->var_id = 'user_id';
+
         }else{ // a visitor
-            $token = session()->get('visitToken13');
+            $token = session()->get('visitToken');
             if(!empty($token)){
                 $visitor = Visitor::where('token',$token)->first();
                 $this->conversation = Conversation::where('visitor_id',$visitor->id)->first();
             }else{
                $visitor = $this->createNewVisitor();
                 // save token in session
-                session()->put('visitToken13',$visitor->token);
+                session()->put('visitToken',$visitor->token);
             }
             $this->user    = $visitor;
             $this->var_id  = 'visitor_id';

5669a62a03dca5d249786dfe0643c2d802e9ee6a
Ahmed <perfect_ahmed@yahoo.com>
2018-06-27 15:06:55 +0300

    audio links

--- resumeApp/app/Http/Controllers/Auth/RegisterController.php
+++ resumeApp/app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
             'lastName' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'username' => 'required|string|min:6|unique:users',
+            'username' => 'required|alpha_dash|string|min:6|unique:users',
             'profession' => 'required',
             'englishLevel' => 'accepted'
         ]);

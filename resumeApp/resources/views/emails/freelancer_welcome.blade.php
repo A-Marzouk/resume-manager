621c3bcb6c970df9ccca4afc62e2334834b53589
Ahmed <perfect_ahmed@yahoo.com>
2018-06-27 15:33:03 +0300

    freelancer welcome e-mail

--- resumeApp/resources/views/emails/freelancer_welcome.blade.php
+++ resumeApp/resources/views/emails/freelancer_welcome.blade.php
@@ -8,7 +8,7 @@
 
         We are now actively promoting your cv to our list of clients currently hiring. <br/><br/>We are currently seeking a suitable company for you to work at for your preferred hourly rate and availability, will be in touch with you as soon as we have a good job offer for you.
 
-        <br/><br/>Please feel free to contact us at anytime with questions that you could have on <b>riz@123workforce.com</b>.  Could we also ask you to please edit your availability of hours should it change over time by signing in and edit hours. With this, we can accurately place and match  you with the most suitable clients.
+        <br/><br/>Please feel free to contact us at anytime with questions that you could have on <b style="color: #0290D8;"><a href="mailto:riz@123 workforce.com">Riz Pasion</a></b>.  Could we also ask you to please edit your availability of hours should it change over time by signing in and edit hours. With this, we can accurately place and match  you with the most suitable clients.
 
         <br/><br/>Wishing you onging success with your current projects !<br/><br/>
 

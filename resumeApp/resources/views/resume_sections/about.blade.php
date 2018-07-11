05473efdc55e223587810d342f72969585f23524
Ahmed <perfect_ahmed@yahoo.com>
2018-07-11 16:27:20 +0300

    resume design 2 ( about done )

--- resumeApp/resources/views/resume_sections/about.blade.php
+++ resumeApp/resources/views/resume_sections/about.blade.php
@@ -1,54 +1,44 @@
 {{-- About me Section --}}
 <div id="about">
     <div class="row">
-        <div class="col-md-5 offset-md-1">
-            <div class="row">
-                <div class="col-md-1 col-1">
-                    <img src="resumeApp/resources/views/customTheme/images/newResume/about_me.png" alt="aboutImg" width="30px;">
-                </div>
-                <div class="col-md-10 col-10">
-                    <div class="aboutText">
-                        ABOUT ME<br>
-                        <div class="row">
-                            <div class="col-12 aboutSubText">
-                                <p>Lang: {{$user->languages}}</p>
-                            </div>
-                            <div class="col-12 aboutSubText">
-                                <p>Email: riz@123workforce.com</p>
-                            </div>
-                        </div>
+        <div class="offset-md-1 col-md-5">
+            <img src="resumeApp/resources/views/customTheme/images/newResume/about_me.png" style="padding-bottom: 10px;" alt="aboutImg" width="30px;">
+            <span class="aboutText">ABOUT ME</span>
+            <div class="aboutText">
+                <div class="aboutSubText">
+                    <div class="row">
+                        <div class="col-md-3">Language:</div>
+                        <div class="col-md-6">  {{$user->languages}}</div>
+                    </div><br/>
+                    <div class="row">
+                        <div class="col-md-3">Email:</div>
+                        <div class="col-md-6">riz@123workforce.com</div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-5">
-            <div class="row">
-                <div class="col-md-1 col-1">
-                    <img src="resumeApp/resources/views/customTheme/images/newResume/what_i_do.png" alt="aboutImg" width="30px;">
-                </div>
-                <div class="col-md-10 col-10">
-                    <div class="aboutText">
-                        WHAT I DO?<br> {{-- 6 primary skills --}}
-                        <div class="row">
-                            <div class="col-md-6 col-6 aboutSubText">
-                                <? $counter = count($primarySkills);
-                                if($counter > 6 ){
-                                    $counter = 6 ;
-                                }
-                                ?>
-                                <? if($counter > 1):?>
-                                <? for($i=0; $i < $counter/2;$i++): ?>
-                                <p><span class="circle"></span>{{$primarySkills[$i]}}</p>
-                                <? endfor;?>
-                            </div>
-                            <div class="col-md-6 col-6 aboutSubText">
-                                <? for($i= $counter-1; $i > ($counter/2) -1 ;$i--): ?>
-                                <p><span class="circle"></span>{{$primarySkills[$i]}}</p>
-                                <? endfor;?>
-                            </div>
-                            <? endif;?>
-                        </div>
+            <img src="resumeApp/resources/views/customTheme/images/newResume/what_i_do.png" style="padding-bottom: 10px;" alt="aboutImg" width="30px;">
+            <span class="aboutText">WHAT I DO?</span>
+            <div class="aboutText">
+                <div class="row">
+                    <div class="col-md-6 col-6 aboutSubText">
+                        <? $counter = count($primarySkills);
+                        if($counter > 6 ){
+                            $counter = 6 ;
+                        }
+                        ?>
+                        <? if($counter > 1):?>
+                        <? for($i=0; $i < $counter/2;$i++): ?>
+                            <p><span class="circle"></span>{{$primarySkills[$i]}}</p>
+                        <? endfor;?>
                     </div>
+                    <div class="col-md-6 col-6 aboutSubText">
+                        <? for($i= $counter-1; $i > ($counter/2) -1 ;$i--): ?>
+                            <p><span class="circle"></span>{{$primarySkills[$i]}}</p>
+                        <? endfor;?>
+                    </div>
+                    <? endif;?>
                 </div>
             </div>
         </div>

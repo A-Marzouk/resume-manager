fef88c3910b89b126df929e0455ca7cb0fbbca00
Ahmed <perfect_ahmed@yahoo.com>
2018-07-16 01:09:02 +0300

    design edits 15/07/2018 talent tablet done

--- resumeApp/resources/views/homepage_sections/talent.blade.php
+++ resumeApp/resources/views/homepage_sections/talent.blade.php
@@ -1,6 +1,6 @@
 <div id="freelanceTalent">
     <div class="row customContainer">
-        <div class="col-md-6">
+        <div class="col-md-5 offset-md-1 offset-lg-0 col-lg-6">
             <div class="secondSectionHeading">
                 Global<br/><span>Freelance Talent</span>
                 <div class="lineDivider"></div>
@@ -10,7 +10,7 @@
                 available for hire - on demand.
             </div>
         </div>
-        <div class="col-md-6 mapImg">
+        <div class="col-md-5 col-lg-6 mapImg">
             <img src="/resumeApp/resources/views/customTheme/images/new_theme/map_freelancer.png"
                  alt="freelancers map" width="100%">
         </div>

a941ad96ff52edd102ae3a21a8f0185409ff3c4b
Ahmed <perfect_ahmed@yahoo.com>
2018-07-16 00:35:06 +0300

    design edits 15/07/2018 main tablet done

--- resumeApp/resources/views/homepage_sections/main.blade.php
+++ resumeApp/resources/views/homepage_sections/main.blade.php
@@ -1,6 +1,6 @@
 <div id="mainSection">
     <div class="row customContainer">
-        <div class="col-md-8">
+        <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-8">
             <div class="mainSectionHeading">
                 Ensure your <span>business success</span> by hiring approved Freelancers.
             </div>
@@ -8,7 +8,7 @@
                 Access our global talent network of professional Freelancers, they are<br/>reliable, qualified
                 and ready to join your team.
             </div>
-            <div class="buttonMain col-md-6">
+            <div class="buttonMain col-lg-6 col-md-6 offset-md-3 offset-lg-0">
                 <? if(auth()->guard('client')->guest()):?>
                 <a class="hireBtn" href="#" data-toggle="modal" data-target="#exampleModalCenter">Hire
                     Talent</a>

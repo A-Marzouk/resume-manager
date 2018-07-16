c160b63b89dde8a7f661a2ba3c9722a9aa9659a3
Ahmed <perfect_ahmed@yahoo.com>
2018-07-16 23:13:10 +0300

    client details button

--- resumeApp/resources/views/admin/usersList.blade.php
+++ resumeApp/resources/views/admin/usersList.blade.php
@@ -102,6 +102,7 @@
                         <th scope="col">#</th>
                         <th scope="col">Full Name</th>
                         <th scope="col">Link to Profile</th>
+                        <th scope="col">View details</th>
                         <th scope="col"></th>
                     </tr>
                     </thead>
@@ -112,6 +113,49 @@
                         <th scope="row">{{$i}}</th>
                         <td>{{$client->name}}</td>
                         <td><a href="{{route('logInAsClient',$client->id)}}" target="_blank">Link to profile</a></td>
+                        <td>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#clientDetails{{$client->id}}">
+                                Details
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="clientDetails{{$client->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">{{$client->name}} - details</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="row">
+                                                <div class="col-md-6"> Name :</div>
+                                                <div class="col-md-6"> {{$client->name}}</div>
+
+                                                <div class="col-md-6"> Email :</div>
+                                                <div class="col-md-6"> {{$client->email}}</div>
+
+                                                <div class="col-md-6"> Agency :</div>
+                                                <div class="col-md-6"> {{$client->agency}}</div>
+
+                                                <div class="col-md-6"> Email Dept :</div>
+                                                <div class="col-md-6"> {{$client->emailDept}}</div>
+
+                                                <div class="col-md-6"> Time zone :</div>
+                                                <div class="col-md-6"> {{$client->timeZone}}</div>
+
+                                                <div class="col-md-6"> Signing up date :</div>
+                                                <div class="col-md-6"> {{ $client->created_at->format('d M Y - H:i:s') }}</div>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
                         <td><a class="btn btn-sm btn-danger" href="{{route('client.delete',$client->id)}}"  onclick="return confirm('Are you sure you want to delete {{$client->name}}?');">Delete</a></td>
                     </tr>
 

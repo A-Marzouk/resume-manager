647b158996f2866922529500a68237abd994538b
Ahmed <perfect_ahmed@yahoo.com>
2018-07-25 13:23:54 +0300

    changes 24-7 (5) freelancer video on front-end

--- resumeApp/resources/views/resume_sections/audio.blade.php
+++ resumeApp/resources/views/resume_sections/audio.blade.php
@@ -32,3 +32,36 @@
         </div>
     </div>
 </div>
+
+<div id="video">
+    <div class="row">
+        <div class="col-md-5 offset-md-1 videoHeading">
+            <img src="resumeApp/resources/views/customTheme/images/newResume/videoIcon.png" alt="video">
+            <span class="quoteText">
+                Video Introduction
+            </span>
+        </div>
+    </div>
+    @if($user->video_file !== null)
+        <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <div class="text-center">
+                <video width="80%" id="videoResume" height="auto" controls>
+                    <source src="{{$user->video_file}}">
+                </video>
+            </div>
+        </div>
+    </div>
+    @elseif($user->video !== null)
+        <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <div class="text-center">
+                <iframe src="{{$user->video}}" frameborder="1" allow="encrypted-media" allowfullscreen width="80%" height="auto">
+
+                </iframe>
+            </div>
+        </div>
+    </div>
+    @endif
+    <hr/>
+</div>

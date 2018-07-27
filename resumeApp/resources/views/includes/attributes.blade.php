f433f8d93457198bd6d6686542a0261b600ab9a0
Ahmed <perfect_ahmed@yahoo.com>
2018-07-27 03:17:45 +0300

    changes 24-7 done except 2 points 2

--- resumeApp/resources/views/includes/attributes.blade.php
+++ resumeApp/resources/views/includes/attributes.blade.php
@@ -7,16 +7,6 @@
         </ul>
     </div>
     <div class="form-group col-md-12">
-        <label for="personal_interests" class="panelFormLabel">Professional interests <span id="tickMarkpersonal_interests" class="d-none"><img src="resumeApp/resources/views/customTheme/images/Shape.png" width="15px;" height="12px;"></span></label>
-        <textarea class="form-control" rows="3" id="personal_interests" name="personal_interests">{{$personal_interests}}</textarea>
-        <small class="panelFormLabel">Your hobbies and/or things you’d personally like to do in the future / How do you spend your free time. </small>
-    </div> <!-- interests -->
-    <div class="form-group col-md-12">
-        <label for="professional_attributes" class="panelFormLabel">Personal attributes <span id="tickMarkprofessional_attributes" class="d-none"><img src="resumeApp/resources/views/customTheme/images/Shape.png" width="15px;" height="12px;"></span></label>
-        <textarea class="form-control" rows="3" id="professional_attributes" name="professional_attributes">{{$professional_attributes}}</textarea>
-        <small class="panelFormLabel">Kindly describe yourself in the workplace. Are you a team player, detail oriented, organized, etc.?</small>
-    </div> <!-- attributes -->
-    <div class="form-group col-md-12">
         <hr>
 
         <label style=" padding: 2px;" class="panelFormLabel">Please choose 6 character skills</label>
@@ -66,6 +56,17 @@
             @endforeach
         </div>
     </div> <!-- char skills -->
+
+    <div class="form-group col-md-12">
+        <label for="personal_interests" class="panelFormLabel">Outside of work , what are your hobbies and interests?<span id="tickMarkpersonal_interests" class="d-none"><img src="resumeApp/resources/views/customTheme/images/Shape.png" width="15px;" height="12px;"></span></label>
+        <textarea class="form-control" rows="3" id="personal_interests" name="personal_interests">{{$personal_interests}}</textarea>
+    </div> <!-- interests -->
+
+    <div class="form-group col-md-12">
+        <label for="professional_attributes" class="panelFormLabel">Please talk in greater detail about your character skills and personal attributes that you have ticked above or other character skills you have.
+            <span id="tickMarkprofessional_attributes" class="d-none"><img src="resumeApp/resources/views/customTheme/images/Shape.png" width="15px;" height="12px;"></span></label>
+        <textarea class="form-control" rows="3" id="professional_attributes" name="professional_attributes">{{$professional_attributes}}</textarea>
+    </div> <!-- attributes -->
 
     <div class="row">
         <div class="buttonMain col-md-4 offset-md-4">

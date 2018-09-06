b11288b1590281e21c56e398d575c37438ea5ad3
Ahmed <perfect_ahmed@yahoo.com>
2018-09-06 15:17:51 +0300

    privacy policy page

---
+++ resumeApp/resources/views/privacy.blade.php
@@ -0,0 +1,205 @@
+@extends('layouts.client-app')
+
+@section('content')
+    <div class="container">
+        <h1 class="pageHeading">Privacy policy</h1><br/>
+
+        <p class="panelFormLabel">
+            This Privacy policy describes how 123 Workforce (123 Workforce) may hold and process your
+            personal data during the course of its activities as a provider of  recruitment services.
+            .<br/><br/>
+
+            123 Workforce is committed to respecting your privacy and has taken steps to be compliant with the
+            General Data Protection Regulation (GDPR), which came into force on 25 May 2018 and is
+            designed to ensure personal data is processed lawfully, fairly, transparently and for specific,
+            explicit and legitimate purposes.
+            We keep our Privacy Policy under review and will advise of updates.<br/><br/>
+
+            <b>Collection of data</b><br/>
+            During the course of its work, 123 Workforce may collect personal data which identifies, or can be use to identify, individuals.
+            This personal data may include information such as name, age, home address, telephone
+            number, fax number, e-mail address, organisation name, job title, business address,
+            occupation, examination records, bank and payment details, education and training details,
+            CPD records, health, accomodation, convictions, application forms and assessment records.
+            Personal data may be collected by a variety of means, including face to face, by telephone,
+            mail, fax, internet and e-mail, at events, and via third parties where you have consented to
+            them sharing it.
+            We may obtain and store information from third party sources.<br/><br/>
+
+
+
+            <b>Use of data</b><br/>
+            Your personal data is held and processed by 123 Workforce in accordance with legal bases set out in data
+            protection law, specifically contract, legitimate interest, and consent, and as explained in
+            this Privacy Policy.
+
+            If you are, or are in the process of assessment to become freelancer with 123 Workforce your personal
+            data may be lawfully processed by 123 Workforce on the basis of contract, as set out at the
+            commencement of your application to become a freelancer.
+            The personal data you supply in respect of your application for registration with 123 Workforce and the
+            relevant clients will (as applicable) be used to;
+            process your enquiry and subsequent application should you decide to proceed to the
+            application stage.<br/><br/>
+
+            Administer your ongoing accreditation and registration as a Freelancer including matters
+            relating to your training needs, performance and history as a Freelancer.
+            ensure you receive and are informed of the supports, including training and out of hours
+            services which you, as a Freelancer with 123 Workforce are entitled.
+            connect you with other Freelancers in the 123 Workforce “family”.
+            communicate any matters which may affect your ability to freelance.
+            Your enquiry, application and subsequent assessment will be held on file as a record of the
+            assessment of your suitability to freelance by 123 Workforce’s assessor and subsequently the relevant Committee.
+            <br/><br/>
+
+            In the interests of transparency and the protection of children and for the specific purpose of
+            making a “match” between client(s) and freelancer, 123 Workforce reserves the right to share your assessment
+            including the personal data therein with our clients
+            123 Workforce’s approval or oversight from approved third parties
+            requires that 123 Workforce retain associated data that will also be shared with the relevant body who
+            may communicate with you on essential matters relating to your suitability or experience of
+            freelancing or indeed of 123 Workforce.
+            123 Workforce has a legitimate interest in keeping its Freelancers informed, in a proportionate way, of
+            our activities at both corporate and local levels. We may therefore lawfully use your personal
+            data to send you periodic notices and information about events and service developments that
+            we believe will be of benefit to you as a Freelancer.
+            Similarly, we also have a legitimate interest in approaching you from time to time to request
+            your involvement in expert surveys and consultations conducted by 123 Workforce to help us fulfil our stated aim of providing a quality freelancing service / platform
+            These legitimate interests apply unless or until you either resign as a Freelancer with 123 Workforce and have completed a service exit interview.
+            Finally, where you have given us express consent, by providing us with your mailing
+            preferences, you may also lawfully receive other communications about our products and
+            services.<br/><br/>
+
+            <b>Employees of 123 Workforce</b><br/>
+            If you are a previous, current or prospective employee of 123 Workforce we may lawfully process your
+            personal data on the basis of contract as set out in individual terms of employment.
+            We will keep and use this data to manage our relationship with you effectively, lawfully and
+            appropriately, during the recruitment process, while you are working for us, at the time when
+            your employment ends and after you have left. Much of the information we hold will have
+            been provided by you, but some may come from other internal sources, such as your manager,
+            or in some cases external sources, such as referees and accreditation bodies.
+            Where necessary, we may keep information relating to your health, which could include
+            reasons for absence. This information is used in order to comply with our health and safety
+            obligations and to administer and manage 123 Workforce and statutory sick pay, maternity pay, etc..
+
+            <br/><br/>
+
+            123 Workforce will only disclose information about you to third parties if we are required by law to do so by law or in order to comply with our contractual duties to you, for instance we may need to pass on certain information to our external payroll provider and pension provider (see
+            ‘Data sharing’ below) or in the event that you leave our employment to provide an appropropriate
+            reference in the event that you have nominated us as a referee.
+            We may also store information provided by people who express an interest in working for us,
+            whether via a formal application or expressed informally.
+
+
+            <b>Contractors to 123 Workforce</b><br/>
+            123 Workforce will retain and process personal data relating to contacts at third party companies
+            contracted to support our activities, as made necessary by contracts agreed. This includes, but
+            is not limited to, technical consultants, data processors, training tutors and service providers.<br/>
+            Users of 123 Workforce website
+            Neither 123 Workforce nor its website (the Site) hosts make use of your IP address, or other session information that is automatically captured by the web server, for any purpose other than
+            system administration and to provide statistics that are used to evaluate the use of the Site.<br/>
+            The Site makes use of ’cookies’, which are small text files that contain a unique identification
+            number written to a user’s hard disk and read by a server programme. Cookies are used
+            generally to keep a record of your details in order to recognise you the next time you visit.<br/>
+            Cookies are used on the Sites to collect information about visits to the Sites. They use Google
+            Analytics, a web analytics service provided by Google Inc. Google Analytics sets cookies in
+            order to evaluate your use of the Sites and compile reports for us on activity on the Sites.<br/> We
+            use the information to compile reports and to help us improve the website. The cookies
+            collect information in an anonymous form only.
+            Google stores the information collected by the cookie on servers in the United States.<br/> Google
+            may also transfer this information to third parties where required to do so by law, or where
+            such third parties process the information on Googles  behalf. Google will not associate your
+            IP address with any other data held by Google. By using the Sites, you consent to the
+            processing of data about you by Google in the manner and for the purposes set out above.<br/>
+            You can opt-out of Google Analytics for Display Advertising and customize Google Display
+            Network ads using the Ad Settings tool. As an added privacy measure, you can use Google
+            Analytics opt-out browser add-on.<br/>
+            Use of the Site does not require you to disclose personal information unless you choose to
+            register an enquiry with 123 Workforce via the Site.<br/> Individual enquiry is required to elect and receive
+            email notification services and updates; proceed with a PDF file download transaction.<br/><br/>
+
+
+            <b>Data sharing</b><br/>
+
+            The owner of personal data held by 123 Workforce will at all times be 123 Workforce. We will never sell your data
+            to third parties and, as data controller; 123 Workforce has taken steps to ensure that personal data
+            remains in a secure environment.<br/>
+            We will only share it with third parties in the case of trusted ‘data processors’, ‘joint data
+            controllers’ or ‘data controllers in common’ with whom we have contracts in place to ensure
+            onward data protection, or where we are required by law to do so.<br/>
+            Data processors used by 123 Workforce
+            Personal data held by 123 Workforce may be processed by trusted third party data processors where we
+            have an appropriate contract in place to ensure onward data protection in line with data
+            protection law. Where data processors are headquartered outside the EU, additional
+            protections have been ensured.<br/>
+            123 Workforce’s main data processors are listed below.<br/>
+            Google is contracted by the EI to provide a database platform – STREAK – to securely
+            store and manage data relating to enquiries, customers and contacts.<br/>
+            Google Analytics - Google is certified under the EU-US Privacy Shield.<br/>
+            Microsoft Corporation provides email services to the EI as well as providing the full suite
+            of Office 365 programs and functions including SharePoint for encrypted file transfer.<br/>
+            Microsoft is certified under the EU-US Privacy Shield.<br/>
+            financial systems and advisers for the payment of staff salaries and other payments, for
+            management and payment of expenses and purchase invoices.<br/><br/>
+
+            123 Workforce as a joint data controller or data controller
+            in common
+            In a small number of cases 123 Workforce is a joint data controller or data controller in common with a
+            partner organisation:<br/>
+            Applications to the Tusla Foster Care Committees require high levels of sharing of
+            personal data in line with the protocols of Tusla, the statutory authority. Freelancers will be
+            fully informed of the process, their freedom to participate in and of timing of such
+            applications.<br/>
+            Working towards and achieving a match between an 123 Workforce Freelancer and a client in need
+            of recruitment requires levels of disclosure of personal information to the Client.<br/>
+            This is under, and protected by, specific protocols established by Tusla and followed by 123 Workforce.<br/><br/>
+
+            <b>Data retention</b><br/>
+            Data is retained by 123 Workforce only for as long as it has legitimate reason to do so. Where your
+            information is no longer required, it is either rendered anonymous for retention or disposed of
+            in a secure manner.<br/>
+            123 Workforce Freelancer data is retained for as long as availability to freelance is ‘current’ (until we
+            are informed that a freelancer wishes to resign or they are retired by 123 Workforce and/or the relevant Recruitment Committee), and for 6 years plus the current year. However, Freelancer records are
+            retained beyond this to enable us to respond to queries relating to the retrospective care of the
+            client.<br/>
+            Customer data is retained for 6 years plus the current year after purchase, in line with
+            Revenue requirements. Records of attendance at events and training are also retained as a
+            matter of record in relation to evidenced training, CPD participation and qualifications
+            gained.<br/>
+            Data relating to past, current and prospective contributors to 123 Workforce’s work are retained for
+            various durations as necessary to fulfil the specific legitimate interests.
+            Employee data will be retained for as long as the employment contract is in place or 6
+            years beyond termination of employment, in line with time limits in law. Details of
+            unsuccessful job applicants will be retained for 1 year after the start date of the unsuccessful
+            applicant, in line with legal requirements.<br/>
+            Data relating to contractors to 123 Workforce will be retained for the duration of the contracted
+            work and 6 years plus the current year beyond, in line with Revenue requirements.
+            Enquiries and enquirers contacts who have no other relationship with 123 Workforce will be retained on our database only if they have given consent within 24 months of being asked,
+            otherwise they will be deleted. Where a contact has unsubscribed, a simple record may be
+            retained for the purposes of ensuring suppression of future email communications.
+            123 Workforce will respond when requested to any data access request in line with data protection law.
+            We will also, if asked, respect the right of an individual to ask to be forgotten entirely in so
+            far as it does not undermine legal requirements.
+            If you believe that any information we are holding on you is incorrect or incomplete, please
+            let us know as soon as possible and we will correct any information found to be incorrect.
+            Contact details
+            <br/><br/><br/>
+            If you have any comments, questions or concerns about how 123 Workforce handles personal data, or in relation
+            to your personal data held by 123 Workforce, please contact:<br/><br/>
+            <a href="mailto:info@123Workforce.com">info@123Workforce.com</a> or write to us at Data Governance Officer, 123 Workforce, 326 Swords
+            Road, Santry, Dublin 9<br/> <br/>
+
+            To contact the Data Protection Commission: Office of the Data Protection Commission, Canal House,
+            Station Road, Portarlington, Co. Laois, R32 AP23, Ireland. Phone <a href="tel: +353 (0761) 104 800"> +353 (0761) 104 800</a> | LoCall 1890
+            25 22 31 | Fax +353 57 868 4757 | email <a href="mailto:info@dataprotection.ie">info@dataprotection.ie</a>.
+
+
+
+
+
+
+
+
+
+        </p>
+    </div>
+@endsection

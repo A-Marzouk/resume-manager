8680bee6ffbaaaad2ec955a8be4a665d9bfdc03e
Ahmed <perfect_ahmed@yahoo.com>
2018-09-17 20:44:05 +0300

    bookings system 3

--- resumeApp/resources/views/payment.blade.php
+++ resumeApp/resources/views/payment.blade.php
@@ -12,51 +12,51 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 col-12">
                 <div class="paymentHeader">
                     Payment Options
                 </div>
                 <div class="paymentOption">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-12">
                             <img src="/resumeApp/resources/views/customTheme/images/checked_method.png" alt="" style="padding-bottom: 2px;padding-right: 6px;width: 23px;">
                             <span class="paymentText font-weight-bold">Stripe</span>
                             <div class="paymentNote NoDecor" style="font-weight: normal; color:#72808E;">
                                 Complete your purchase securely with <a href="https://stripe.com" target="_blank">Stripe</a>.
                             </div>
                         </div>
-                        <div class="col-md-6 text-right">
+                        <div class="col-md-6 col-12 stripeImage">
                             <img src="/resumeApp/resources/views/customTheme/images/stripe-logo.png" alt="stripe logo" width="90" height="24">
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-12">
                 <div class="paymentHeader">
                     Payment Details
                 </div>
                 <div class="paymentDetails">
                     <div class="row">
-                        <div class="col-md-6 paymentText" style="color: #697786;">
+                        <div class="col-md-6 col-6 paymentText" style="color: #697786;">
                             Hourly rate :
                         </div>
-                        <div class="col-md-6 text-right paymentText font-weight-bold" style="color: #697786;">
+                        <div class="col-md-6 col-6 text-right paymentText font-weight-bold" style="color: #697786;">
                             {{intval($freelancer->userData->salary)+5}} $
                         </div>
                     </div>
                     <div class="row border-bottom" style="padding-top: 20px; padding-bottom: 40px;">
-                        <div class="col-md-6 paymentText" style="color: #697786;">
+                        <div class="col-md-6 col-6 paymentText" style="color: #697786;">
                             {{$hours}} hours
                         </div>
-                        <div class="col-md-6 text-right paymentText font-weight-bold" style="color: #697786;">
+                        <div class="col-md-6 col-6 text-right paymentText font-weight-bold" style="color: #697786;">
                             x {{$hours}}
                         </div>
                     </div>
                     <div class="row" style="padding-top: 20px; padding-bottom: 30px;">
-                        <div class="col-md-6 paymentText" style="color: #697786; font-size: 18px;">
-                            Total to Pay :
+                        <div class="col-md-6 col-12 paymentText totalPayText" style="color: #697786; font-size: 18px;">
+                            Total to Pay <span class="d-none d-md-inline-flex">:</span>
                         </div>
-                        <div class="col-md-6 text-right paymentText font-weight-bold" style="color: #697786; font-size: 18px;">
+                        <div class="col-md-6 col-12 text-right totalPayText paymentText font-weight-bold" style="color: #697786; font-size: 18px;">
                             {{(intval($freelancer->userData->salary)+5) * $hours}} $
                         </div>
                     </div>
@@ -84,7 +84,7 @@
                                     document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
                                 </script>
                                 <div class="row text-center">
-                                    <div class="col-10 offset-1" style="padding-top: 0; margin-top: 0;">
+                                    <div class="col-md-10 offset-md-1" style="padding-top: 0; margin-top: 0;">
                                         <button class="btn-block paymentBtn cursorPointerOnHover" type="submit">Confirm and Pay</button>
                                     </div>
                                 </div>

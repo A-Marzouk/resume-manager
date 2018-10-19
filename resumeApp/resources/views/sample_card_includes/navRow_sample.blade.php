36d6e2419def92d63d60b4ea13e74f1acbf76118
Ahmed <perfect_ahmed@yahoo.com>
2018-10-19 16:08:48 +0300

    mobile 6 - sample page

--- resumeApp/resources/views/sample_card_includes/navRow_sample.blade.php
+++ resumeApp/resources/views/sample_card_includes/navRow_sample.blade.php
@@ -1,5 +1,5 @@
 <div class="row navRow">
-    <div class="col-md-2 offset-md-3 col-4 text-center" style="border-right:1px solid #EBEDEF;">
+    <div class="col-md-2 offset-md-1 col-4 text-center" style="border-right:1px solid #EBEDEF;">
         <div class="navTab showPortfolio active NoDecor" id="showPortfolio{{$freelancer->id}}{{$value['id']}}">
             <a href="javascript:void(0)">
                 Portfolio

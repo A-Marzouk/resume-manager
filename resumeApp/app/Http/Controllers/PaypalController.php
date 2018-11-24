0c87ca389b63488c912cc4708e2848b5c51e70f5
Ahmed <perfect_ahmed@yahoo.com>
2018-11-25 01:19:55 +0200

    paypal send/receive functions

---
+++ resumeApp/app/Http/Controllers/PaypalController.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ahmed
+ * Date: 11/24/2018
+ * Time: 8:48 PM
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
+use PayPal\Api\Amount;
+use PayPal\Api\Currency;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Payout;
+use PayPal\Api\PayoutItem;
+use PayPal\Api\PayoutSenderBatchHeader;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+use PayPal\auth\OAuthTokenCredential;
+use PayPal\Exception\PayPalConnectionException;
+use PayPal\Rest\ApiContext;
+
+class PaypalController extends Controller
+{
+    private $_api_context ;
+    public function __construct()
+    {
+        $this->middleware('admin');
+        $paypal_config      = Config::get('paypal');
+        $this->_api_context = new ApiContext( new OAuthTokenCredential(
+            $paypal_config['client_id'],
+            $paypal_config['secret_id']
+        ));
+        $this->_api_context->setConfig($paypal_config['settings']);
+    }
+
+    public function showForm(){
+        return view('paypalForm');
+    }
+
+    public function showSendForm(){
+        return view('paypal_send_form');
+    }
+
+    public function payWithPaypal(Request $request){
+        // this function is to receive payments from clients through paypal.
+        // this can be easily used with Stripe payments.
+        // the user needs to use to pay with paypal - then login and confirm.
+        $payer = new Payer();
+        $payer->setPaymentMethod("paypal");
+
+        $item_1 = new Item();
+        $item_1->setName('Item 1') /** item name **/
+        ->setCurrency('USD')
+            ->setQuantity(1)
+            ->setPrice($request->amount); /** unit price **/
+
+        $itemList = new ItemList();
+        $itemList->setItems(array($item_1));
+
+
+        $amount = new Amount();
+        $amount->setCurrency("USD")
+            ->setTotal($request->amount);
+
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($itemList)
+            ->setDescription("Payment description")
+            ->setInvoiceNumber(uniqid());
+
+        $redirectUrls = new RedirectUrls();
+        $redirectUrls->setReturnUrl(env('APP_URL').'/paypal/status')
+            ->setCancelUrl(env('APP_URL').'/paypal/status');
+
+        $payment = new Payment();
+        $payment->setIntent("sale")
+            ->setPayer($payer)
+            ->setRedirectUrls($redirectUrls)
+            ->setTransactions(array($transaction));
+
+        try {
+            $payment->create($this->_api_context);
+        }catch (PayPalConnectionException $ex) {
+            if (Config::get('app.debug')) {
+                return redirect('/')->with('errorMessage','Connection timeout');
+            } else {
+                return redirect('/')->with('errorMessage','Some error occur, sorry for inconvenient');
+            }
+        }
+
+        Session::put('paypal_payment_id',$payment->getId());
+
+        $approvalUrl = $payment->getApprovalLink();
+
+        if (isset($approvalUrl)) {
+            return redirect($approvalUrl);
+        }
+
+        return redirect('/')->with('errorMessage', 'Unknown error occurred');
+//        return $payment;
+    }
+
+    public function getPayPalPaymentStatus(){
+        $paymentId = Session::get('paypal_payment_id');
+        Session::forget('paypal_payment_id');
+
+        if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
+            return redirect('/')->with('errorMessage', 'Unknown error occurred');
+        }
+
+        $payment = Payment::get($paymentId, $this->_api_context);
+        $execution = new PaymentExecution();
+        $execution->setPayerId(Input::get('PayerID'));
+        /**Execute the payment **/
+        $result = $payment->execute($execution, $this->_api_context);
+        if ($result->getState() == 'approved') {
+            return redirect('/')->with('successMessage','Payment success');
+        }
+        return redirect('/')->with('errorMessage','Payment failed');
+
+    }
+
+
+
+    // now we need functions to send payout payments
+
+    public function sendPaymentByPayPal(Request $request){
+        $payouts = new Payout();
+        $senderBatchHeader = new PayoutSenderBatchHeader();
+        $senderBatchHeader->setSenderBatchId(uniqid())
+            ->setEmailSubject("You have a Payout!");
+        $senderItem = new PayoutItem();
+        $senderItem->setRecipientType('Email')
+            ->setNote('Thanks for your patronage!')
+            ->setReceiver($request->email)
+            ->setSenderItemId(uniqid())
+            ->setAmount(new Currency([
+                'value'=>$request->amount,
+                'currency'=>'USD'
+            ]));
+
+        $payouts->setSenderBatchHeader($senderBatchHeader)
+            ->addItem($senderItem);
+
+        try {
+            $output = $payouts->createSynchronous($this->_api_context);
+
+        } catch (PayPalConnectionException $ex) {
+            return redirect('/')->with('errorMessage','Could not complete the payment');
+        }
+
+        return redirect('/')->with('successMessage','Payment success!');
+    }
+
+}
---
+++ resumeApp/resources/views/paypal_send_form.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.affiliate-app')
+
+@section('content')
+    <div class="container">
+        <div class="pageHeading"> PayPal Payments</div><br/>
+        <form action="{{route('submit.paypal.send.form')}}" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group col-md-6 offset-md-3">
+                <label for="amount" class="panelFormLabel">Amount to pay * <small> (USD) </small>
+                    </span>
+                </label>
+                <input type="text" class="form-control panelFormInput" id="amount" name="amount" required>
+            </div> <!-- amount to pay -->
+
+            <div class="form-group col-md-6 offset-md-3">
+            <label for="email" class="panelFormLabel">Email *
+            </span>
+            </label>
+            <input type="text" class="form-control panelFormInput" id="email" name="email" required>
+            </div> <!-- paypal email -->
+
+
+            <div class="buttonMain whiteBg col-md-4 offset-md-4">
+                <button class="hireBtn btn-block hire" type="submit">Pay via Paypal</button>
+            </div>
+        </form>
+
+    </div>
+@endsection

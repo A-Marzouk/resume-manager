75c6ce3a060b66e44e8c11d8b6fadc6f0eddebcf
Ahmed <perfect_ahmed@yahoo.com>
2019-03-29 02:08:44 +0200

    menu first

--- resumeApp/resources/views/client/dashboard.blade.php
+++ resumeApp/resources/views/client/dashboard.blade.php
@@ -1,5 +1,20 @@
 @extends('client.layouts.client_frame')
-@section('content')
+    @section('content')
+        <div class="client_dashboard_content">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="menu-block active row">
+                        <div class="imageContainer">
+                            <img src="/resumeApp/public/images/client/menu_icons/campaign_manager.png" alt="icon">
+                        </div>
+                        <div class="menu-block-name">
+                            Campaign manager
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
 
-
-@endsection
+                </div>
+            </div>
+        </div>
+    @endsection

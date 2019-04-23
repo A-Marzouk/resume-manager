72eb175e107107e557ca2cfd53ffd2266dbb8624
Ahmed <perfect_ahmed@yahoo.com>
2019-04-23 19:12:49 +0300

    front-end - account edit - 2

--- resumeApp/resources/assets/js/components/client/dashboardComponents/my_account/MyAccountEdit.vue
+++ resumeApp/resources/assets/js/components/client/dashboardComponents/my_account/MyAccountEdit.vue
@@ -1,6 +1,107 @@
 <template>
-    <div class="account-info-edit">
-        test
+    <div class="account-info-edit-wrapper">
+        <nav class="navbar navbar-light fixed-top client_navbar">
+            <div class="backBtn">
+                <a href="/client#/my-account">
+                    <img src="/resumeApp/public/images/client/arrow_back.png" alt="back-icon">
+                </a>
+                ACCOUNT INFORMATION
+            </div>
+        </nav>
+
+        <div class="account-info-edit dashboard-box">
+            <div class="account-info-edit-heading dashboard-box-heading">
+                <div class="left">
+                    <img src="/resumeApp/public/images/client/my_account/info_40px.png" alt="info icon">
+                    <span>
+                        EDIT ACCOUNT INFORMATION
+                    </span>
+                </div>
+            </div>
+
+            <div class="account-edit-section">
+                <div class="account-edit-section-heading">
+                    BASIC INFORMATION
+                </div>
+                <div class="account-edit-section-inputs">
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="account-edit-section">
+                <div class="account-edit-section-heading">
+                    PREFERRED CONTACT
+                </div>
+                <div class="account-edit-section-inputs flex">
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="account-edit-section">
+                <div class="account-edit-section-heading">
+                    PASSWORD
+                </div>
+                <div class="account-edit-section-inputs">
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                    <div class="faq-question-input account-edit-input">
+                        <div class="faq-input">
+                            <input type="text" name="faq" placeholder="Write a frequently asked question" >
+                            <img src="/resumeApp/public/images/client/campaign_activity/close_black.png" alt="delete icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 

6cfec829fe7232816eb1926f8571c3a55752f404
nasrin <n.mosh4yedi@gmail.com>
2019-04-26 22:50:05 +0430

    invoice-show front-end

---
+++ resumeApp/resources/views/client/payments/payment_show_invoice.blade.php
@@ -0,0 +1,10 @@
+@extends('client.layouts.client_frame')
+
+@section('content')
+
+    <div id="paymentShowInvoice">
+        <payment-show-invoice>
+        </payment-show-invoice>
+    </div>
+
+@endsection

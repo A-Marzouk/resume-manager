3a6e1cf053692b8eb9d4e7cfc11aaed7a099de7b
Ahmed <perfect_ahmed@yahoo.com>
2019-10-21 19:30:20 +0300

    LinkedIn Import (updated2)

--- resumeApp/public/js/app.js
+++ resumeApp/public/js/app.js
@@ -66407,14 +66407,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 
@@ -66427,8 +66419,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     },
     data: function data() {
         return {
-            clientID: '865ff2s2qvpkzb',
-            redirectURL: 'https://123workforce.com/freelancer/linkedin',
             slides: [],
             numberOfSlides: this.calculateNumberOfSlides(),
             skills: this.freelancer.skills,
@@ -66761,31 +66751,7 @@ var render = function() {
                 )
               ]),
               _vm._v(" "),
-              _c("div", { staticClass: "importBtn NoDecor" }, [
-                _c("span", [_vm._v("Import : ")]),
-                _vm._v(" \n                    "),
-                _c(
-                  "a",
-                  {
-                    attrs: {
-                      href:
-                        "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=" +
-                        _vm.clientID +
-                        "&redirect_uri=" +
-                        _vm.redirectURL +
-                        "&state=987654321&scope=r_basicprofile",
-                      id: "importBehanceData"
-                    }
-                  },
-                  [
-                    _vm._v(
-                      "\n                        LinkedIn\n                    "
-                    )
-                  ]
-                )
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "col-md-2" }),
+              _c("div", { staticClass: "col-md-5" }),
               _vm._v(" "),
               _vm._m(0),
               _vm._v(" "),
--- resumeApp/resources/views/freelancer/edit_form.blade.php
+++ resumeApp/resources/views/freelancer/edit_form.blade.php
@@ -47,7 +47,7 @@ $freelancer = auth()->user();
                     <span>Import : </span>&nbsp;
                     <a href="javascript:void(0)"id="importBehanceData" data-toggle="modal" data-target="#behanceDataModal">Behance </a> &nbsp; | &nbsp;
                     <a
-                            :href="'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=' + clientID + '&redirect_uri=' + redirectURL + '&state=987654321&scope=r_basicprofile' "
+                            href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={{$clientID}}&redirect_uri={{$redirectURL}}&state=987654321&scope=r_basicprofile"
                             id="importBehanceData">
                         LinkedIn
                     </a>

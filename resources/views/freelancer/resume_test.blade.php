8898e338b0ca31d0b2c8b612463b99c1d219f832
Ahmed <perfect_ahmed@yahoo.com>
2019-11-27 15:55:51 +0200

    freelancer resume

--- resources/views/freelancer/resume_test.blade.php
+++ resources/views/freelancer/resume_test.blade.php
@@ -11,39 +11,8 @@
 </head>
 <body>
 
-  <?
-    $skillsArray = array();
-    foreach ($skills as $skill) {
-
-      array_push($skillsArray, [
-        "id" => $skill->id,
-        "name" => $skill->label,
-        "type" => $skill->skillType->name
-      ]);
-    }
-
-    $userData = array(
-      "monthly_salary" => $user_data->monthly_salary,
-      "available_hours_per_week" => $agent->available_hours_per_week,
-      "photo" => $user_data->avatar,
-      "job_title" => $user_data->job_title,
-      "salary" => $agent->hourly_rate
-    );
-  ?>
-  
-  <div id="freelancerResume">
-    <freelancer-resume :freelancer="{{ json_encode(array(
-      "id" => $user->id,
-      "firstName" => $user->firstName,
-      "lastName" => $user->lastName,
-      "projects" => array(),
-      "username" => $username
-    )) }}"
-      :skills="{{ json_encode($skillsArray) }}"
-      :works_history="{{ json_encode($worksHistory) }}"
-      :educations_history="{{ json_encode($educationsHistory) }}"
-      :user_data="{{ json_encode($userData) }}"
-      ></freelancer-resume>
+  <div id="freelancerResumeLongV2" class="d-flex justify-content-center">
+    <freelancer-resume-long-v2 :freelancer="{{$freelancer}}"></freelancer-resume-long-v2>
   </div>
 
   @include('layouts.includes.scripts')

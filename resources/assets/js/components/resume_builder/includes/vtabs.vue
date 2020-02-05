f07d3e4249a00995c5313f710147ecb29fd9b889
0x6A6F736564616E3130 <josedan10@users.noreply.github.com>
2020-02-05 06:20:09 -0400

    merge eduardo branch

---
+++ resources/assets/js/components/resume_builder/includes/vtabs.vue
@@ -0,0 +1,118 @@
+<template>
+    <v-tab
+        :href="`#tab-${index}`"
+        :ripple="false" 
+        @click="clicked"
+    >
+        {{ item }}
+    </v-tab>
+</template>
+
+<script>
+export default {
+    name: "vTabNames",
+    props: {
+        index:{
+            type: Number
+        },
+        item: {
+            type: String
+        }
+    },
+    methods: {
+        clicked() {
+            this.$emit('clicked');
+        }        
+    }
+}
+</script>
+<style lang="scss">
+/** Styles general tabs */
+
+.theme--light.v-tabs__bar{
+    background-color: transparent;
+}
+
+.v-tabs{
+    max-height: 85px;
+    font-family: 'Noto Sans';
+
+    .v-tabs__div{
+        margin-right: 8%;
+        
+        &:last-child{
+            margin: 0;
+        }
+    }
+
+    .v-tabs__wrapper{
+        position: relative;
+
+        .v-tabs__container{
+            justify-content: flex-start;
+        }
+
+        &::after{
+            content: "";
+            height: 4px;
+            background: #C9CFF8;
+            position: absolute;
+            width: 100%;
+            left: 0px;
+            bottom: -1px;
+            z-index: -1;
+        }
+
+    }
+    .v-tabs__slider{
+        height: 3px;
+        
+    }
+    .v-tabs__item{
+        text-transform: capitalize;
+        padding: 0;
+        text-align: left;
+        font-weight: bold;
+        font-size: 30px;
+        color: #505050;
+        text-decoration: none;
+        min-width: 152px;
+    }
+
+    .v-tabs__slider-wrapper{
+        min-width: 152px;
+    }
+    
+
+    &.tabs_inside{
+
+        margin: 40px auto 0px auto;
+        max-width: 98%;
+
+        .v-tabs__div{
+            margin: 0;
+            min-width: 150px;
+        }
+
+        .v-tabs__wrapper{
+
+            .v-tabs__container{
+                justify-content: space-between;
+            }
+        }
+        .v-tabs__item{
+            font-size: 21px;
+            font-family: 'Noto Sans';
+            line-height: 28px;
+            color: #3C3748;
+            min-width: auto;
+            text-transform: none;
+ 
+        }
+
+        .v-tabs__slider-wrapper{
+            min-width: 158px;
+        }
+    }
+}
+</style>
--- resources/assets/js/components/resume_builder/tabs/edit_tabs/Profile.vue
+++ resources/assets/js/components/resume_builder/tabs/edit_tabs/Profile.vue
@@ -1,17 +1,457 @@
 <template>
-    <div>
-        <h3>
-            Profile tab
-        </h3>
+    <div class="profile-hold">
+        <div class="section-title">
+            <div class="title-light">Edit</div>
+            <h2>Profile</h2>
+        </div>
+        <v-tabs
+            v-model="cTabProfile"
+            :slider-color="tabColor"
+            height="85"
+        >
+            <vTabNames v-for="(nametab,i) in tabsProfile" :key="i" :index="i" :item="nametab" @clicked="clicked_tab(nametab)" />
+        </v-tabs>
+        <v-tabs-items v-model="cTabProfile">
+            <v-tab-item value="tab-0">
+                <div class="hold-edit">
+                    <img class="user-cover" src="/images/resume_builder/default-user.jpg" alt="">
+                    <div class="upload-section">
+                        <h5>Change profile photo</h5>
+                        <p>Only use images that are greater than 500 pixels in both height and width.</p>
+                        <input type="file" class="input_photoFile"> 
+                    </div>
+                    <form class="form-edit_profile">
+                        <div class="input-field">
+                            <label for="fullname">My full-name</label>
+                            <input type="text" placeholder="" name="fullname">
+                        </div>
+                        <div class="input-field">
+                            <label for="email">Email adress</label>
+                            <input type="email" placeholder="" name="email">
+                        </div>
+                        <div class="input-field">
+                            <label for="designation">Designation</label>
+                            <input type="text" placeholder="" name="designation">
+                        </div>
+                        <div class="input-field">
+                            <label for="phone">Phone</label>
+                            <input type="tel" placeholder="" name="phone">
+                        </div>
+                        <div class="input-field">
+                            <label for="aboutmyself">About myself <i class="hint-message">Maximum 80 words</i></label>
+                            <textarea name="aboutmyself"></textarea>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="btn-blue"><img src="/images/resume_builder/profile/icon-save.png">Save all information</a>
+                        </div>
+                    </form>
+                </div>
+            </v-tab-item>
+            <v-tab-item value="tab-1">
+                <v-tabs
+                    v-model="cTabLinks"
+                    :slider-color="tabColor"
+                    height="85"
+                    class="tabs_inside"
+                    centered="true"
+                >
+                    <vTabNames v-for="(nametab,i) in tabsLinks" :key="i" :index="i" :item="nametab" />
+                </v-tabs>
+                <v-tabs-items v-model="cTabLinks">
+                    <v-tab-item value="tab-0">
+                        
+                        <div class="hold-tab">
+                            <div class="input-field">
+                                <label for="profilelink">My profile link</label>
+                                <input type="text" placeholder="" name="profilelink">
+                            </div>
+                            <div class="quick-opts">
+                                <a href=""><span class="edit"></span></a>
+                                <a href=""><span class="copy"></span></a>
+                                <a href=""><span class="share"></span></a>
+                            </div>
+                            <a href="#" class="btn-blue"><img src="/images/resume_builder/profile/icon-save2.png">Save this new link</a>
+
+                        </div>
+                        
+                    </v-tab-item>
+                    <v-tab-item value="tab-1">
+                        <div class="hold-tab social">
+                            <div class="options-wrap">
+                                <a href="" class="btn-outline">Add new link</a>
+                                <a href="" class="btn-outline">Auto import</a>
+                            </div>
+                            <div class="list-links">
+                                <ul>
+                                    <li>
+                                        <span class="move">
+                                            <a href="" class="up-item"></a>
+                                            <a href="" class="down-item"></a>
+                                        </span>
+                                        <span>
+                                            https://www.Behance.com/hachib_rahman_/
+                                        </span>
+                                        <select class="mdb-select md-form">
+                                            <option value="" disabled selected>Choose your option</option>
+                                            <option value="" data-icon="https://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="rounded-circle">
+                                                example
+                                                1</option>
+                                            <option value="" data-icon="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="rounded-circle">
+                                                example
+                                                2</option>
+                                            <option value="" data-icon="https://mdbootstrap.com/img/Photos/Avatars/avatar-3.jpg" class="rounded-circle">
+                                                example
+                                            1</option>
+                                        </select>                                    
+                                    </li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                           
+                        </div>
+                    </v-tab-item>
+                    <v-tab-item value="tab-2">
+                        <v-card>
+                            tres
+                        </v-card>
+                    </v-tab-item>
+                    <v-tab-item value="tab-3">
+                        <v-card>
+                            cuatro
+                        </v-card>
+                    </v-tab-item>
+                </v-tabs-items>
+            </v-tab-item>
+            <v-tab-item value="tab-2">
+                <v-card flat>
+                    <v-card-text>
+                        tab 3
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item value="tab-3">
+                <v-card flat>
+                    <v-card-text>
+                        tab 4
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
     </div>
 </template>
-
 <script>
+    import vTabNames from '../../includes/vtabs.vue';
+
     export default {
-        name: "Profile"
+        name: "Profile",
+        components: {
+            vTabNames
+        },
+        data() {
+            return {
+                cTabProfile: 'tab-0',
+                cTabLinks: null,
+                tabColor: '#001CE2',
+                tabsProfile: [
+                    'personal',
+                    'links',
+                    'languages',
+                    'location'
+                ],
+                tabsLinks: [
+                    'My profile link',
+                    'Social link',
+                    'Portfolio link',
+                    'Payment link'
+                ]
+            }
+        },
+        methods: {
+            clicked_tab(name) {
+                
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scope>
+
+    .title-blue{
+        font-family: 'Nexa';
+        font-size: 60px;
+        line-height: 67px;
+        color: #001CE2;
+    }
+
+    $colorPrimary: #001CE2;
+    $colorGray: #505050;
+
+
+    .profile-hold{
+        justify-content: flex-start;
+        width: 1363px;
+        flex-flow: column;
+    }
+
+    .hold-edit{
+        font-family: 'Noto Sans';
+        padding: 40px 20px;
+
+        .user-cover{
+            max-width: 144px;
+            min-height: 144px;
+            border-radius: 100%;
+            border: 5px solid #fff;
+            box-shadow: -5px 16px 30px rgba(90, 90, 90, 0.12);
+            background: #ddd;
+        }
+        .upload-section{
+            display: inline-block;
+            vertical-align: middle;
+            margin-left: 22px;
+
+            h5{
+                
+                font-size: 22px;
+                font-weight: bold;
+                color: $colorPrimary
+            }
+            p{
+                font-size: 16px;
+                color: $colorGray;
+            }
+        }
+    }
+    .input_photoFile{
+        position: relative;
+        text-decoration: none;
+        font-size: 15px;
+        max-width: 203px;
+        color: #fff;
+        min-height: 48px;
+        padding: 0 24.5px;
+        background: $colorPrimary;
+        display: inline-block;
+        border-radius: 8px;
+
+        &:hover{
+            opacity: .8;
+            color: #fff;
+            text-decoration: none;
+        }
+    }
+    .input_photoFile::-webkit-file-upload-button {
+        visibility: hidden;
+    }
+    .input_photoFile::before {
+        content: "Choose photo now";
+        outline: none;
+        white-space: nowrap;
+        -webkit-user-select: none;
+        cursor: pointer;
+        line-height: 47px;
+        position: absolute;
+        background: url('/images/resume_builder/profile/icon-file.png') center left no-repeat;
+        padding-left:22px;
+    }
+
+    .form-edit_profile{
+        display: flex;
+        justify-content: center;
+        flex-flow: column;
+        margin-top: 40px;
+
+        .actions{
+            display: flex;
+            max-width: 490px;
+            margin-top: 12.5px;
+        }
+    }
+
+     .input-field{
+        flex-flow: column;
+        display: flex;
+        max-width: 490px;
+        margin-bottom: 31.5px;
+
+        label{
+            font-size: 22px;
+            line-height: 30px;
+            color: #505050;
+            font-family: 'Noto Sans';
+            font-weight: 300;
+
+            .hint-message{
+                font-size: 18px;
+                color: #1F5DE4;
+                float: right;
+                font-style: normal;
+            }
+        }
+
+        input{
+            min-height: 54.6px;
+            border: 1.5px solid #505050;
+            border-radius: 8px;
+            padding: 0px 0px 0px 16.8px;
+            color: #A5A5A5;
+            font-size: 15px;
+            font-family: 'Noto Sans';
+            font-weight: 500;
+            outline: none;
+        }
+
+        textarea{
+            min-height: 181px;
+            padding: 23px 25px;
+            font-size: 22px;
+            font-family: 'Noto Sans';
+            font-weight: 500;
+            outline: none;
+            color: #A5A5A5;
+            border: 1.5px solid #505050;
+            border-radius: 8px;
+        }
+    
+    }
+    
+
+    .btn-blue{
+        text-decoration: none;
+        font-size: 15px;
+        min-width: 203px;
+        max-width: 216px;
+        color: #fff;
+        min-height: 51px;
+        padding: 0 15.8px 0 21.4px;
+        background: $colorPrimary;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 8px;
+        font-family: 'Noto Sans';
+
+        img{
+            margin-right: 10.5px;
+        }
+
+        &:hover{
+            opacity: .8;
+            color: #fff;
+            text-decoration: none;
+        }
+    }
+
+    .hold-tab{
+        justify-content: flex-start;
+        display: flex;
+        align-items: center;
+        margin-top: 55px;
+
+        .input-field{
+            max-width: 100%;
+            min-width: 50.8%;
+
+            label{
+                font-size: 23px;
+                font-weight: 500;
+                color: #3C3748;
+            }
+
+            input{
+                font-size: 22px;
+                min-height: 76px;
+                color: #505050;
+            }
+        }
+
+        .btn-blue{
+            min-width: 294px;
+            min-height: 75px;
+            margin-left: 62px;
+            font-size: 23px;
+            font-weight: 500;
+            border: 1.5px solid #1F5DE4;
+        }
+
+        &.social{
+            flex-flow: column;
+            align-items: flex-start;
+        }
+    }
+
+    .quick-opts{
+        background: #F6F6F6;
+        padding: 18.5px 30px;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 29px;
+        border-radius: 8px;
+
+        a{
+            text-decoration: none;
+            margin-right: 37px;
+
+            &:last-child{
+                margin: 0;
+            }
+
+            .edit{
+                background: url('/images/resume_builder/profile/icon-edit.png') no-repeat;
+                width: 44px;
+                height: 39px;
+                display: block;                
+            }
+            .copy{
+                background: url('/images/resume_builder/profile/icon-copy.png') no-repeat;
+                width: 33.7px;
+                height: 39px;
+                display: block;                
+            }
+            .share{
+                background: url('/images/resume_builder/profile/icon-share.png') no-repeat;
+                width: 44px;
+                height: 39px;
+                display: block;                
+            }
+        }
+
+        
+    }
+
+    /** Social tab */
+    .btn-outline{
+        min-width: 226px;
+        min-height: 62px;
+        font-family: 'Noto Sans';
+        font-size: 21px;
+        font-weight: 500;
+        color: #001CE2;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #1F5DE4;
+        border-radius: 8px;
+        text-decoration: none;
+        outline: none;
+        background: #fff;
+
+        &::before{
+            content: "";
+            width: 31.4px;
+            height: 31.4px;
+            background: url('/images/resume_builder/profile/icon-plus.png');
+            margin-right: 12px;
+        }
+
+        &:hover{
+            text-decoration: none;
+        }
+
+        +.btn-outline{
+            margin-left: 46px;
+        }
+    }
 
 </style>

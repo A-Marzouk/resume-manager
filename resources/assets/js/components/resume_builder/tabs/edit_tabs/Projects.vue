62b849bf5b2d03ce0433486260e2d9dce617bee7
0x6A6F736564616E3130 <josedan10@users.noreply.github.com>
2020-02-11 00:12:23 -0400

    mainn page for projects

--- resources/assets/js/components/resume_builder/tabs/edit_tabs/Projects.vue
+++ resources/assets/js/components/resume_builder/tabs/edit_tabs/Projects.vue
@@ -6,14 +6,36 @@
         </div>
 
         <div class="content d-flex">
-            <form-view1 v-if="formStep === 1"></form-view1>
+            <!-- Include router view here -->
+            <div class="wrapper">
+                <div class="text-hint">Only use images that are greater than 500 pixels in both height and 300 pixels in width.</div>
+
+                <div class="btns-wrapper d-flex align-items-center">
+                    <button class="btn outline">
+                        <svg-vue class="icon add-project-icon" icon="add-icon"></svg-vue> <b>Add a new project</b>
+                    </button>
+                    <button class="btn outline">
+                        <svg-vue class="icon add-project-icon" icon="add-icon"></svg-vue> <b>Auto import</b>
+                    </button>
+                </div>
+                <div class="projects-container d-flex">
+                    <div class="project" v-for="project in projects" :key="project.name">
+                        <img :src="project.mainImage" alt="">
+                        <a class="d-flex justify-content-center align-items-center remove-icon" href="javascript:;" @click="removeProject">
+                            <svg-vue icon="remove-icon"></svg-vue>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <form-view1 v-if="formStep === 1"></form-view1>
             <form-view2 v-else-if="formStep === 2"></form-view2>
             <form-view3 v-else></form-view3>
             <div class="step-indicator">
                 <a href="javascript:void;" @click="prevStep"><svg-vue class="step-arrow-icon" icon="left-arrow-icon"></svg-vue></a>
                 <span @click="setStep(step)" class="step-dot" :class="{active: step === formStep}" v-for="step in 3" :key="step + 'step'"></span>
                 <a href="javascript:void;" @click="nextStep"><svg-vue class="step-arrow-icon" icon="right-arrow-icon"></svg-vue></a>
-            </div>
+            </div> -->
         </div>
     </section>
 </template>
@@ -31,7 +53,17 @@ export default {
         FormView3
     },
     data: () => ({
-        formStep: 1
+        formStep: null,
+        projects: [
+            {
+                name: 'Project 1',
+                mainImage: '/images/resume_builder/project1.png'
+            },
+            {
+                name: 'Project 2',
+                mainImage: '/images/resume_builder/project2.png'
+            }
+        ]
     }),
     methods: {
         nextStep () {
@@ -50,6 +82,52 @@ export default {
 <style scoped lang="scss">
 .content {
     position: relative;
+}
+
+.projects-container {
+    margin-top: 32px;
+
+}
+
+.project {
+    position: relative;
+    margin-right: 87px;
+
+    .remove-icon {
+        opacity: 0;
+        transition: all .5s ease;
+        background: white;
+        position: absolute;
+        right: 14px;
+        top: 14px;
+        width: 53px;
+        height: 53px;
+        box-shadow: -3px 10px 15px #e5e5e5;
+        border-radius: 5px;
+
+        svg {
+            width: 23px;
+            height: 23px;
+        }
+    }
+
+    &:hover .remove-icon {
+        opacity: 1;
+        transition: all .5s ease;
+    }
+}
+
+.btns-wrapper {
+    margin-top: 26px;
+
+    button {
+        margin-right: 12px;
+    }
+}
+
+.add-project-icon {
+    width: 31px;
+    height: 31px;
 }
 
 .step-indicator {

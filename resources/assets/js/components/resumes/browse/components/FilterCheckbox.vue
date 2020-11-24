aa4cc5172a09e973a24b21444aea8e88b8c41f15
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-11-24 16:19:22 +0100

    Fix issues and Add FilterCheckbox to FilterFreelancers

---
+++ resources/assets/js/components/resumes/browse/components/FilterCheckbox.vue
@@ -0,0 +1,70 @@
+<template>
+	<button class="filter-checkbox" :class="{'--checked':value, '--prefix':prefix}" type="button" @click="$emit('input', !value)" v-text="title"></button>
+</template>
+
+<script>
+export default {
+	name: "FilterCheckbox",
+	props: {
+		title: { type: String },
+		value: { type: Boolean, default: false },
+		prefix: { type: Boolean, default: false },
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/variables";
+@import "../scss/media-queries";
+
+.filter-checkbox {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-left: 14px;
+	padding-right: 14px;
+	border-radius: 6px;
+	border: 1px solid transparent;
+	background-color: $moonraker14;
+	transition: all 0.2s;
+	height: 37px;
+	white-space: nowrap;
+	color: $midnightblue;
+	font-size: 12px;
+	margin-right: 15px;
+	margin-bottom: 5px;
+	min-width: 100px;
+	&.--prefix {
+		margin-left: 15px;
+	}
+	&.--checked {
+		color: #f7f7fe;
+		background: $midnightblue;
+	}
+	&:last-child {
+		margin-right: unset;
+	}
+	&:focus {
+		outline: none;
+		border-color: $lynch50;
+	}
+}
+
+@include lg {
+	.filter-checkbox {
+		font-size: 20px;
+		height: 50px;
+		padding-left: 28px;
+		padding-right: 28px;
+	}
+}
+
+@include xl {
+	.filter-checkbox {
+		font-size: 26px;
+		height: 74px;
+		padding-left: 50px;
+		padding-right: 50px;
+	}
+}
+</style>
--- resources/assets/js/components/resumes/browse/components/FilterFreelancers.vue
+++ resources/assets/js/components/resumes/browse/components/FilterFreelancers.vue
@@ -1,31 +1,115 @@
 <template>
 	<div class="filter-freelancers">
-		<FilterDropdown title="Job title" v-model="filters.jobTitle" :options="jobTitles" />
+		<div class="filter-freelancers__right-side">
+			<FilterDropdown title="Job title" v-model="filters.jobTitle" :options="jobTitles" />
+			<FilterCheckbox prefix title="Primary skills" v-model="filters.primarySkills" />
+			<FilterCheckbox title="Second skills" v-model="filters.SecondSkills" />
+		</div>
+		<div class="filter-freelancers__left-side">
+			<FilterDropdown title="Sort By" v-model="sortBy" muted :options="sortOptions" />
+		</div>
 	</div>
 </template>
 
 <script>
+import FilterCheckbox from "./FilterCheckbox";
 import FilterDropdown from "./FilterDropdown";
 
 export default {
 	name: "FilterFreelancers",
-	components: { FilterDropdown },
+	components: { FilterCheckbox, FilterDropdown },
 	data() {
 		return {
 			filters: {
 				jobTitle: "",
+				primarySkills: true,
+				SecondSkills: false,
 			},
-			jobTitles: ["Front-end Engineer", "UI/UX", "Designer"],
+			sortBy: "",
+			sortOptions: [
+				{ id: 1, value: "Job titles" },
+				{ id: 2, value: "Skills" },
+				{ id: 3, value: "Portfolio" },
+			],
+			jobTitles: [
+				{ id: 1, value: "Front-end engineer" },
+				{ id: 2, value: "UI/UX" },
+				{ id: 3, value: "Designer" },
+				{ id: 4, value: "Back-end engineer" },
+				{ id: 5, value: "Senior developer" },
+				{ id: 6, value: "Project manager" },
+			],
 		};
 	},
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/variables";
+@import "../scss/media-queries";
+
 .filter-freelancers {
 	display: flex;
 	align-items: center;
-	justify-content: center;
-	margin-top: 28px;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	margin: 28px auto 0;
+	max-width: 986px;
+	padding-bottom: 5px;
+	.filter-freelancers__right-side {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		flex-wrap: wrap;
+	}
+	.filter-freelancers__left-side {
+		width: 100%;
+		display: flex;
+		padding-top: 34px;
+		.filter-dropdown {
+			margin-left: auto;
+		}
+	}
+}
+
+@include sm {
+	.filter-freelancers {
+		.filter-freelancers__right-side {
+			justify-content: flex-start;
+		}
+	}
+}
+
+@include md {
+	.filter-freelancers {
+		.filter-freelancers__right-side {
+			width: auto;
+		}
+		.filter-freelancers__left-side {
+			width: auto;
+			padding-top: unset;
+		}
+	}
+}
+
+@include xl {
+	.filter-freelancers {
+		max-width: 1467px;
+		.filter-freelancers__right-side {
+		}
+		.filter-freelancers__left-side {
+		}
+	}
+}
+
+@include xxl {
+	.filter-freelancers {
+		max-width: 1700px;
+		.filter-freelancers__right-side {
+		}
+		.filter-freelancers__left-side {
+		}
+	}
 }
 </style>

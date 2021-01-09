a7be2904c125773721bbf41a65d05019fafaceb6
Ahmed-pc <perfect_ahmed@yahoo.com>
2021-01-09 08:20:49 +0200

    #homepage
    - set up emails on homepage | logo update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="/images/new_homepage/logo.svg" class="logo" alt="Civie Logo">
+<img src="/images/new_homepage/logo.svg" class="logo" alt="Logo">
 @else
 {{ $slot }}
 @endif
